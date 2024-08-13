d3838e6050678cae5757c7592bf8a6d6ef249dc3
gmoli45 <gmoli45@wgu.edu>
2024-08-13 15:00:54 -0700

    Load trucks

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import math
 from My_HashMap import HashMap
 from Package import Package
 from Truck import Truck
+from datetime import datetime
 
 # create hashmap instance
 myHashMap = HashMap()
@@ -33,6 +34,8 @@ def loadPackageData(fileName):
 
 loadPackageData('WGUPS Package File.csv')
 
+
+# read distance table
 def loadDistanceData(filename):
     with open(filename) as distanceTable:
         distance_data = csv.reader(distanceTable, delimiter=',')
@@ -40,4 +43,15 @@ def loadDistanceData(filename):
 
     return data_read
 
+
 distances = loadDistanceData('WGUPS Address-Distance Table.csv')
+
+# instantiate truck objects
+truck1 = Truck()
+truck2 = Truck()
+truck3 = Truck()
+
+truck1.load([1, 13, 14, 15, 16, 19, 20, 29, 30, 31, 34, 37, 40], 'Driver A', '8:00', 'idk yet')
+truck2.load([2, 3, 4, 5, 6, 7, 8, 18, 25, 28, 32, 36, 38], 'Driver B', '9:05', 'idk yet')
+truck3.load([9, 10, 11, 12, 17, 21, 22, 23, 24, 26, 27, 33, 35, 39], '', '', '')
+
